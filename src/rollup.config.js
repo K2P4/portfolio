58a74be28d4ff32da83b745133d0cfd6d986fd69
Phyo Thu Ya <vik8312@gmail.com>
2024-03-30 19:51:13 +0630

    update

---
+++ src/rollup.config.js
@@ -0,0 +1,13 @@
+/** @format */
+
+// rollup.config.js
+import resolve from "@rollup/plugin-node-resolve";
+
+export default {
+	input: "src/index.js",
+	output: {
+		file: "dist/bundle.js",
+		format: "cjs",
+	},
+	plugins: [resolve()],
+};
